20939cf1291db67682aca6a58e7226544b914bff
David Bacon <dpb@davidbacon.co.uk>
2022-06-09 17:30:20 +0100

    changed to cloudinary development, changed to cl image tags, reseeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,60 +16,60 @@ greenspace1 = Greenspace.new(
   name: "Shoreditch Park"
 )
 
-file = URI.open("https://res.cloudinary.com/dpk3pz6zs/image/upload/v1654788098/greenspace1b_rubiad.jpg")
-greenspace1.photo.attach(io: file, filename: "Shoreditchpark1.jpg", content_type: "image/jpg")
+file1 = URI.open("https://res.cloudinary.com/dpk3pz6zs/image/upload/v1654788098/greenspace1b_rubiad.jpg")
+greenspace1.photos.attach(io: file1, filename: "Shoreditchpark1.jpg", content_type: "image/jpg")
 greenspace1.save
 
-file = URI.open("https://res.cloudinary.com/dpk3pz6zs/image/upload/v1654788093/greenspace1a_kdiwyq.jpg")
-greenspace1.photo.attach(io: file, filename: "Shoreditchpark2.jpg", content_type: "image/jpg")
+file2 = URI.open("https://res.cloudinary.com/dpk3pz6zs/image/upload/v1654788093/greenspace1a_kdiwyq.jpg")
+greenspace1.photos.attach(io: file2, filename: "Shoreditchpark2.jpg", content_type: "image/jpg")
 greenspace1.save
 
-file = URI.open("https://res.cloudinary.com/dpk3pz6zs/image/upload/v1654788093/greenspace1c_ziwnii.jpg")
-greenspace1.photo.attach(io: file, filename: "Shoreditchpark3.jpg", content_type: "image/jpg")
+file3 = URI.open("https://res.cloudinary.com/dpk3pz6zs/image/upload/v1654788093/greenspace1c_ziwnii.jpg")
+greenspace1.photos.attach(io: file3, filename: "Shoreditchpark3.jpg", content_type: "image/jpg")
 greenspace1.save
 
 puts "created #{greenspace1.name}!!!"
 
-greenspace2 = Greenspace.create(
-  location: "Hoxton",
+greenspace2 = Greenspace.new(
+  address: "Hoxton",
   description: "Nice park few, too many dogs",
-  photo: 'park5_landscape.jpg',
   average_rating: 3,
   name: "Hoxton Park"
 )
+file1.rewind
+greenspace2.photos.attach(io: file1, filename: "Shoreditchpark1.jpg", content_type: "image/jpg")
+greenspace2.save!
 
-file = URI.open("https://res.cloudinary.com/dpk3pz6zs/image/upload/v1654788098/greenspace1b_rubiad.jpg")
-greenspace2.photo.attach(io: file, filename: "HoxtonPark1.jpg", content_type: "image/jpg")
+file2.rewind
+greenspace2.photos.attach(io: file2, filename: "HoxtonPark2.jpg", content_type: "image/jpg")
 greenspace2.save
 
-file = URI.open("https://res.cloudinary.com/dpk3pz6zs/image/upload/v1654788093/greenspace2a_kdiwyq.jpg")
-greenspace2.photo.attach(io: file, filename: "HoxtonPark2.jpg", content_type: "image/jpg")
-greenspace2.save
-
-file = URI.open("https://res.cloudinary.com/dpk3pz6zs/image/upload/v1654788093/greenspace2c_ziwnii.jpg")
-greenspace2.photo.attach(io: file, filename: "HoxtonPark3.jpg", content_type: "image/jpg")
+file3.rewind
+greenspace2.photos.attach(io: file3, filename: "HoxtonPark3.jpg", content_type: "image/jpg")
 greenspace2.save
 
 puts "created #{greenspace2.name}!!!"
 
-greenspace3 = Greenspace.create(
-  location: "Haggerston",
+greenspace3 = Greenspace.new(
+  address: "Haggerston",
   description: "very peaceful and serene space",
-  photo: 'park6_landscape.jpg',
   average_rating: 5,
   name: "Haggerston Park"
 )
 
-file = URI.open("https://res.cloudinary.com/dpk3pz6zs/image/upload/v1654788098/greenspace1b_rubiad.jpg")
-greenspace3.photo.attach(io: file, filename: "Haggerston1.jpg", content_type: "image/jpg")
+file1.rewind
+
+greenspace3.photos.attach(io: file1, filename: "Haggerston1.jpg", content_type: "image/jpg")
 greenspace3.save
 
-file = URI.open("https://res.cloudinary.com/dpk3pz6zs/image/upload/v1654788093/greenspace3a_kdiwyq.jpg")
-greenspace3.photo.attach(io: file, filename: "Haggerston2.jpg", content_type: "image/jpg")
+file2.rewind
+
+greenspace3.photos.attach(io: file2, filename: "Haggerston2.jpg", content_type: "image/jpg")
 greenspace3.save
 
-file = URI.open("https://res.cloudinary.com/dpk3pz6zs/image/upload/v1654788093/greenspace3c_ziwnii.jpg")
-greenspace3.photo.attach(io: file, filename: "Haggerston3.jpg", content_type: "image/jpg")
+file3.rewind
+
+greenspace3.photos.attach(io: file3, filename: "Haggerston3.jpg", content_type: "image/jpg")
 greenspace3.save
 
 puts "created #{greenspace3.name}!!!"
