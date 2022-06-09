519fc63754000e25bf7a970da3462898c67a2214
David Bacon <dpb@davidbacon.co.uk>
2022-06-09 18:14:11 +0100

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-ActiveRecord::Schema[7.0].define(version: 2022_06_09_132841) do
-
 ActiveRecord::Schema[7.0].define(version: 2022_06_09_144431) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
