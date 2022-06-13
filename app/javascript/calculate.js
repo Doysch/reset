4e25614a909fd3b7f910ef120a2654b4481a9a4d
David Bacon <dpb@davidbacon.co.uk>
2022-06-13 18:40:18 +0100

    added js pseudocode

--- app/javascript/calculate.js
+++ app/javascript/calculate.js
@@ -1,1 +1,9 @@
 console.log("Hello world");
+
+// 1. Fetch the co-ordinates from input
+
+
+
+// 2. Fetch the co-ordinates from greenspace location
+// 3. Place into Mapbox API
+// 4. Fetch the figure from API and replace the hardcoded time on index page
