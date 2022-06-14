1956bd4725628de7a8534d1dee1179095099c557
David Bacon <dpb@davidbacon.co.uk>
2022-06-14 10:09:27 +0100

    added walk time to index page, fixed navbar on new

--- app/javascript/calculate.js
+++ app/javascript/calculate.js
@@ -1,9 +1,29 @@
-console.log("Hello world");
 
-// 1. Fetch the co-ordinates from input
+// 1. Get the co-ordinates from input
+// 2. Get the co-ordinates from greenspace location
+
+const latLng = document.getElementById('walk-calculator');
+// const arr = latLng.dataset.mapSearchValue;
+// console.log(arr);
+
+let latStarting = latLng.attributes[3].value[0]
+console.log(latStarting);
+// let lngStarting = latLng.waypoints[0].location[1];
+// console.log(lngStarting);
+// let latEnding = latLng.waypoints[1].location[0];
+// console.log(latEnding);
+// let lngEnding = latLng.waypoints[1].location[0];
+// console.log(lngEnding);
 
 
-
-// 2. Fetch the co-ordinates from greenspace location
 // 3. Place into Mapbox API
+
+// fetch('https://api.mapbox.com/directions/v5/mapbox/walking/{coordinates}')
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+
+
+
+
+
 // 4. Fetch the figure from API and replace the hardcoded time on index page
